f0774067273fdc2415540202830df5add0e921fd
cryptottat <cryptottat@example.com>
2025-11-15 23:15:24 +0900

    Add BUY button to header linking to pump.fun

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,6 +12,7 @@ function Header() {
           <a href="https://x.com/conduitx402" className="nav-link" data-text="x">x</a>
           <a href="https://docs.conduitx402.com" className="nav-link" data-text="docs">docs</a>
           <a href="javascript:void(0)" className="nav-link nav-link-disabled" data-text="app" onClick={(e) => { e.preventDefault(); e.stopPropagation(); }}>app</a>
+          <a href="https://pump.fun/coin/BMH9iKhbkMzN9DvyvcsQKmEndV7kNUEszBf9wSNHpump" className="nav-link nav-link-buy" data-text="BUY" target="_blank" rel="noopener noreferrer">BUY</a>
         </nav>
       </div>
     </header>
